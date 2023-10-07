5eb99869b056a581306b85412b96a7c92ae493fc
Slava Gornostal <slava.gornostal@gmail.com>
2023-10-07 22:19:33 +0300

    add data generator / decrease model params

--- train2.py
+++ train2.py
@@ -2,10 +2,11 @@ import os
 import cv2
 import numpy as np
 import tensorflow as tf
+from keras.callbacks import ModelCheckpoint
 from keras.layers import Input
 from keras.models import Model, save_model
-
 import model2
+from tripplet_generator import TripletGenerator
 
 
 def triplet_loss(y_true, y_pred, margin=1.0):
@@ -28,68 +29,25 @@ def load_and_preprocess(image_path):
 if __name__ == '__main__':
     root_folder = 'fingerprint-v5-master'
 
-    # Lists to store data
-    anchors = []
-    positives = []
-    negatives = []
-
-    individuals = os.listdir(root_folder)
-
-    for i in range(len(individuals)):
-        current_individual = individuals[i]
-        for hand in ['L', 'R']:
-            anchor_path = os.path.join(root_folder, current_individual, hand)
-            positive_hand = 'R' if hand == 'L' else 'L'
-            positive_path = os.path.join(root_folder, current_individual, positive_hand)
-
-            # Load anchor and positive images
-            for anchor_img_name in os.listdir(anchor_path):
-                img_path = os.path.join(anchor_path, anchor_img_name)
-                anchor_img = load_and_preprocess(img_path)
-                if anchor_img is None:
-                    continue
-
-                for positive_img_name in os.listdir(positive_path):
-                    positive_img = load_and_preprocess(os.path.join(positive_path, positive_img_name))
-                    if positive_img is None:
-                        continue
-                    # Get a negative sample
-                    neg_individual = np.random.choice([ind for j, ind in enumerate(individuals) if j != i])
-                    neg_hand = np.random.choice(['L', 'R'])
-                    negative_path = os.path.join(root_folder, neg_individual, neg_hand)
-                    negative_img_name = np.random.choice(os.listdir(negative_path))
-                    negative_img = load_and_preprocess(os.path.join(negative_path, negative_img_name))
-                    if negative_img is None:
-                        continue
-
-                    # Append to lists
-                    anchors.append(anchor_img)
-                    positives.append(positive_img)
-                    negatives.append(negative_img)
-
-    # Convert to numpy arrays for training
-    anchors = np.array(anchors).reshape(-1, 192, 192, 1)
-    positives = np.array(positives).reshape(-1, 192, 192, 1)
-    negatives = np.array(negatives).reshape(-1, 192, 192, 1)
+    embedding_model = model2.create_unet_model((192, 192, 1))
+    print(embedding_model.summary())
 
     input_anchor = Input(shape=(192, 192, 1))
     input_positive = Input(shape=(192, 192, 1))
     input_negative = Input(shape=(192, 192, 1))
-
-    embedding_model = model2.create_unet_model((192, 192, 1))
-    print(embedding_model.summary())
 
     embedding_anchor = embedding_model(input_anchor)
     embedding_positive = embedding_model(input_positive)
     embedding_negative = embedding_model(input_negative)
 
     output = tf.keras.layers.concatenate([embedding_anchor, embedding_positive, embedding_negative], axis=1)
-
     triplet_model = Model([input_anchor, input_positive, input_negative], output)
     triplet_model.compile(optimizer='adam', loss=triplet_loss)
 
-    y_dummy = np.empty((anchors.shape[0], 3 * 128))
-    triplet_model.fit([anchors, positives, negatives], y_dummy, epochs=12, batch_size=8)
+    checkpoint = ModelCheckpoint('model_weights_best.h5', save_best_only=True, monitor='loss')
 
-    saved_model_path = os.path.join(".", 'saved_model2')
+    triplet_gen = TripletGenerator(root_folder, 32)
+    triplet_model.fit(triplet_gen, epochs=64)
+
+    saved_model_path = os.path.join(".", 'saved_model')
     save_model(triplet_model, saved_model_path)
---
+++ tripplet_generator.py
@@ -0,0 +1,62 @@
+from keras.utils import Sequence
+import os
+import numpy as np
+import cv2
+
+
+class TripletGenerator(Sequence):
+    def __init__(self, root_folder, batch_size):
+        self.root_folder = root_folder
+        self.individuals = os.listdir(root_folder)
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.ceil(len(self.individuals) * 2 / self.batch_size))  # 2 for both hands
+
+    def load_and_preprocess(self, path):
+        img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        if img is None:
+            return None
+        img = cv2.resize(img, (192, 192))
+        img = img / 255.0
+        return img
+
+    def __getitem__(self, index):
+        anchors = []
+        positives = []
+        negatives = []
+
+        start_idx = index * self.batch_size
+        end_idx = min((index + 1) * self.batch_size, len(self.individuals) * 2)
+
+        for i in range(start_idx, end_idx):
+            ind_idx = i // 2
+            current_individual = self.individuals[ind_idx]
+            hand = 'L' if i % 2 == 0 else 'R'
+
+            # Paths
+            anchor_path = os.path.join(self.root_folder, current_individual, hand)
+            positive_hand = 'R' if hand == 'L' else 'L'
+            positive_path = os.path.join(self.root_folder, current_individual, positive_hand)
+
+            # Randomly select images
+            anchor_img_name = np.random.choice(os.listdir(anchor_path))
+            positive_img_name = np.random.choice(os.listdir(positive_path))
+            neg_individual = np.random.choice([ind for j, ind in enumerate(self.individuals) if j != ind_idx])
+            neg_hand = np.random.choice(['L', 'R'])
+            negative_img_name = np.random.choice(os.listdir(os.path.join(self.root_folder, neg_individual, neg_hand)))
+
+            # Load images
+            anchor_img = self.load_and_preprocess(os.path.join(anchor_path, anchor_img_name))
+            positive_img = self.load_and_preprocess(os.path.join(positive_path, positive_img_name))
+            negative_img = self.load_and_preprocess(
+                os.path.join(self.root_folder, neg_individual, neg_hand, negative_img_name))
+
+            if anchor_img is None or positive_img is None or negative_img is None:
+                continue
+
+            anchors.append(anchor_img)
+            positives.append(positive_img)
+            negatives.append(negative_img)
+
+        return [np.array(anchors), np.array(positives), np.array(negatives)], np.zeros((len(anchors),))
