78b198dbe5cb15eba2f8f02933f1425355dca8a7
Slava Gornostal <slava.gornostal@gmail.com>
2023-10-13 16:35:23 +0300

    update model

--- model.py
+++ model.py
@@ -1,49 +1,96 @@
-from keras.layers import Input, Conv2D, MaxPooling2D, Add, Dense, Lambda, GlobalAveragePooling2D, \
-    BatchNormalization, ReLU
+from keras.layers import Input, Conv2D, MaxPooling2D, Add, Dense, Lambda, Activation, SeparableConv2D, \
+    BatchNormalization, GlobalAveragePooling2D
 from keras.models import Model
 import tensorflow as tf
 
 
-def res_block(x, filters, kernel_size=3, stride=1):
-    shortcut = x
-    # First convolution
-    x = Conv2D(filters, kernel_size=kernel_size, strides=stride, padding="same")(x)
+def entry_flow(inputs):
+    x = Conv2D(32, 3, strides=2, padding='same')(inputs)
     x = BatchNormalization()(x)
-    x = ReLU()(x)
-    # Second convolution
-    x = Conv2D(filters, kernel_size=kernel_size, strides=1, padding="same")(x)
+    x = Activation('relu')(x)
+
+    x = Conv2D(64, 3, padding='same')(x)
     x = BatchNormalization()(x)
-    # Adjusting the shortcut for addition if necessary
-    if stride != 1 or shortcut.shape[-1] != filters:
-        shortcut = Conv2D(filters, kernel_size=1, strides=stride, padding="same")(shortcut)
-        shortcut = BatchNormalization()(shortcut)
-    # Adding the shortcut to the output
-    x = Add()([x, shortcut])
-    x = ReLU()(x)
+    x = Activation('relu')(x)
+
+    previous_block_activation = x
+
+    for size in [128, 256, 728]:
+        x = Activation('relu')(x)
+        x = SeparableConv2D(size, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+
+        x = Activation('relu')(x)
+        x = SeparableConv2D(size, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+
+        x = MaxPooling2D(3, strides=2, padding='same')(x)
+
+        residual = Conv2D(size, 1, strides=2, padding='same')(previous_block_activation)
+
+        x = Add()([x, residual])
+        previous_block_activation = x
+
+    return x
+
+
+def middle_flow(x, num_blocks=8):
+    previous_block_activation = x
+
+    for _ in range(num_blocks):
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+
+        x = Activation('relu')(x)
+        x = SeparableConv2D(728, 3, padding='same')(x)
+        x = BatchNormalization()(x)
+
+        x = Add()([x, previous_block_activation])
+        previous_block_activation = x
+
+    return x
+
+
+def exit_flow(x):
+    previous_block_activation = x
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(728, 3, padding='same')(x)
+    x = BatchNormalization()(x)
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(1024, 3, padding='same')(x)
+    x = BatchNormalization()(x)
+
+    x = MaxPooling2D(3, strides=2, padding='same')(x)
+
+    residual = Conv2D(1024, 1, strides=2, padding='same')(previous_block_activation)
+    x = Add()([x, residual])
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(728, 3, padding='same')(x)
+    x = BatchNormalization()(x)
+
+    x = Activation('relu')(x)
+    x = SeparableConv2D(1024, 3, padding='same')(x)
+    x = BatchNormalization()(x)
+
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(96, activation='relu', name='template_output')(x)
+
     return x
 
 
 def create_embedding_model(input_shape):
-    input_img = Input(shape=input_shape)
+    inputs = Input(shape=input_shape)
+    xception = exit_flow(middle_flow(entry_flow(inputs)))
 
-    x = Conv2D(64, (7, 7), strides=2, padding='same', activation='relu')(input_img)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-
-    # Residual Blocks
-    x = res_block(x, 64)
-    x = res_block(x, 64)
-    x = res_block(x, 128, stride=2)
-    x = res_block(x, 128)
-    x = res_block(x, 256, stride=2)
-    x = res_block(x, 256)
-    x = res_block(x, 512, stride=2)
-    x = res_block(x, 512)
-
-    x = GlobalAveragePooling2D()(x)
-
-    template_output = Dense(128, activation='relu', name='template_output')(x)
     # L2 Normalization
-    normalized_template = Lambda(lambda x: tf.math.l2_normalize(x, axis=-1), name='normalized_template')(
-        template_output)
+    normalized_template = Lambda(lambda x: tf.math.l2_normalize(x, axis=-1), name='normalized_template')(xception)
 
-    return Model(inputs=input_img, outputs=normalized_template)
+    return Model(inputs=inputs, outputs=normalized_template)
